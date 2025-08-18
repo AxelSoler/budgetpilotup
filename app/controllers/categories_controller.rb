8101921c7048d0865ec1e8d8158943336b4aecc2
Axel <axelss369@gmail.com>
2025-08-18 21:34:56 +0000

    Update and Delete category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,6 +18,21 @@ class CategoriesController < ApplicationController
 		end
 	end
 
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to @category, notice: 'Category updated successfully.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path, notice: 'Category deleted successfully.'
+  end
+
 	private
 
 	def category_params
