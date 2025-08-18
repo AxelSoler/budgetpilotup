02edf50d514d9bfa13c0e0985b9fa99763b73a10
Axel <axelss369@gmail.com>
2025-08-18 18:10:57 +0000

    Minor changes to auth style + redirect from home if authenticated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@ class HomeController < ApplicationController
   allow_unauthenticated_access only: [:index]
 
   def index
+    if authenticated?
+      redirect_to dashboard_index_path
+    else
+      render :index
+    end
   end
 end
