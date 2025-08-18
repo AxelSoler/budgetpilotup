d4684e37616846ce84afe5d56945d9c0cfde21da
Axel <axelss369@gmail.com>
2025-08-18 19:57:24 +0000

    List and create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,26 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+    @category.user = Current.user
+
+		if @category.save
+			redirect_to @category, notice: 'Category created successfully.'
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def category_params
+		params.require(:category).permit(:name, :description)
+	end
 end
