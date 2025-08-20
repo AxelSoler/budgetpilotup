b827a5b188ae002ab02e3147f6f3f7e2ebd5205b
Axel <axelss369@gmail.com>
2025-08-20 19:13:56 +0000

    Add rails_chart gem

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,22 @@
 class DashboardController < ApplicationController
 
   def index
-    @transactions = Current.user.transactions.includes(:category).order(date: :desc).limit(5)
-    @balance = @transactions.sum(:amount)
+    scope = Current.user.transactions
+
+    @transactions = scope.includes(:category).order(date: :desc).limit(10)
+
+    @balance = scope.sum(:amount)
+
+    @balance_over_time = scope.group(:date).sum(:amount)
+
+    @kind_split = {
+      "Income"   => scope.income.sum(:amount),
+      "Expenses" => -scope.expense.sum(:amount)
+    }
+
+    @expenses_by_category = scope.expense
+                                 .joins(:category)
+                                 .group('categories.name')
+                                 .sum(Arel.sql('ABS(transactions.amount)'))
   end
 end
