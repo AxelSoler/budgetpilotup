61047e48f4483d77c4e4833c3372246e59d20a69
Axel <axelss369@gmail.com>
2025-09-01 22:46:38 +0000

    Update last transactions limit in dashboard to 7

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def index
     scope = Current.user.transactions
 
-    @transactions = scope.includes(:category).order(date: :desc).limit(10)
+    @transactions = scope.includes(:category).order(date: :desc).limit(7)
 
     @balance = scope.sum(:amount)
   end
